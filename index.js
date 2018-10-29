dc4cf25e522cf0f1d9d7379814e09e4a0d4820f4
Cristian Leiva <cleiva94.cl@gmail.com>
2018-10-29 13:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   } 
-  if (string === string.toUpperCase())) {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
   if (string === "I love you, Grandma.") {
