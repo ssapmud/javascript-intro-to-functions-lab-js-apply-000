d8d3de800c93e6034784234ae80c1e36c3db8347
Cristian Leiva <cleiva94.cl@gmail.com>
2018-10-29 13:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
-};
+  console.log(string.toLowerCase())
+}r
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
